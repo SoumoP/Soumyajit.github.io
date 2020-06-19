0b98c455158a0ecf1fb2f081563d5ca2d44326bb
Soumyajit Podder <poddersoumyajit@gmail.com>
2020-06-19 22:49:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+window.addEventListener('mousemove', function(e){
+    var xCoord=e.x;
+    var yCoord=e.y;
+    if(screen.width>600){
+        if(yCoord < 112 && yCoord > 5)
+        {
+            var realNav = document.getElementById("real-nav");
+            realNav.style.transform= "translateY(0px)";
+            var frame = document.getElementById("face");
+            frame.style.opacity = "0%";
+            var heading = document.getElementById("heading0");
+            heading.style.transform= "translateY(-60px)";
+            var blur = document.getElementById("wallpaper");
+            blur.style.filter= "blur(3px)";
+        }
+        else{
+            var realNav = document.getElementById("real-nav");
+            realNav.style.transform= "translateY(-172.8px)";
+            var frame = document.getElementById("face");
+            frame.style.opacity ="100%";
+            var blur = document.getElementById("wallpaper");
+            blur.style.filter= "blur(0px)";
+        }
+    }
+    else{
+        if(yCoord < 112 && yCoord > 5)
+        {
+            var realNav = document.getElementById("real-nav");
+            realNav.style.transform= "translateY(0px)";
+            var frame = document.getElementById("face");
+            frame.style.opacity = "0%";
+            var heading = document.getElementById("heading0");
+            heading.style.transform= "translateY(-60px)";
+            var blur = document.getElementById("wallpaper");
+            blur.style.filter= "blur(3px)";
+        }
+        else{
+            var realNav = document.getElementById("real-nav");
+            realNav.style.transform= "translateY(-230px)";
+            var frame = document.getElementById("face");
+            frame.style.opacity ="100%";
+            var blur = document.getElementById("wallpaper");
+            blur.style.filter= "blur(0px)";
+        }
+    }
+})
+
